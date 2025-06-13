67db8d1f7c56b8478ca99acf43c473f6b6e4d2ea
CrossFode <149864968+CrossFode@users.noreply.github.com>
2025-06-13 21:56:25 +0700

    Add CI workflow for Laravel

--- database/migrations/2025_01_06_154713_create_edit_profile_table.php
+++ database/migrations/2025_01_06_154713_create_edit_profile_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('edit_profile', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('account')->onDelete('cascade');
+$table->foreign('user_id')->references('id')->on('accounts')->onDelete('cascade');
             $table->string('phone_number')->nullable();
             $table->string('profile_picture')->nullable();
             $table->enum('gender', ['male', 'female', 'other'])->nullable();
