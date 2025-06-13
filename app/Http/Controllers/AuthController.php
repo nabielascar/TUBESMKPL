3466e410f6fe3e9d17eb9ef64cd6c95ceb966b58
WildanHF <whadi611@gmail.com>
2025-06-14 01:21:32 +0700

    test-fail-1

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,10 +10,12 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
+     public function login(Request $request)
     {
         return response()->json(['error' => 'forced fail'], 500);
     }
+    
+    
 }
 
 
