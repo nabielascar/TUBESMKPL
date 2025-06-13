475925ed8e209feddb4c58ca1e81f97c2e7bc003
WildanHF <whadi611@gmail.com>
2025-06-14 01:49:41 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,11 +10,29 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
-{
-    return response()->json(['error' => 'forced fail'], 500);
-}
     
+    public function login(){
+        return views('Auth.login');
+    }
+
+    public function register(){
+        return view('Auth.register');
+    }
+
+    public function verifikasi(){
+        return views('Auth.verifikasi');
+    }
+
+    public function resetPassword(){
+        return views('Auth.resetPass');
+    }
+    
+    
+
+    public function login(Request $request)
+    {
+    return response()->json(['error' => 'forced fail'], 500);
+    }
     
 }
 
